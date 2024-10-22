9c691b85e403e120a8646e83abac32f7e8f149e0
mohammad-mursalin <mursalinlamon@gmail.com>
2024-10-22 14:55:36 +0600

    demo spring boot project for jwt

---
+++ src/main/java/com/mursalin/spring_security_series_2/SpringSecuritySeries2Application.java
@@ -0,0 +1,13 @@
+package com.mursalin.spring_security_series_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySeries2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecuritySeries2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mursalin/spring_security_series_2/SpringSecuritySeries2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mursalin.spring_security_series_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySeries2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
